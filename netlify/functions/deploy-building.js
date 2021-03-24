53347f763af20c21d9768dab66383b06945ece5f
kokaneka <kapeel.kokane@capillarytech.com>
2021-03-24 14:28:58 +0530

    added logs to build function

--- netlify/functions/deploy-building.js
+++ netlify/functions/deploy-building.js
@@ -1,6 +1,13 @@
 const fetch = require('node-fetch');
 
 exports.handler = async (event, context) => {
-  const resp = await fetch('https://v1.nocodeapi.com/kokaneka/telegram/OtoWBxzkiUHeAbge/sendText?text=building', { method: 'POST'});
+
+  console.log('starting the build')
+  try {
+    console.log('hitting the telegram API')
+    const resp = await fetch('https://v1.nocodeapi.com/kokaneka/telegram/OtoWBxzkiUHeAbge/sendText?text=building', { method: 'POST'});
+  } catch(e) {
+    console.log('error while hitting the telegram API', JSON.stringify(e));
+  }
   console.log('hit the API!')
 }
