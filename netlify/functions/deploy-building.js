059505e1e4e049b646af79ec36f1bcc7a1e4be53
kokanek <kapeel.kokane@gmail.com>
2021-03-26 17:48:31 +0530

    modified the building hook

--- netlify/functions/deploy-building.js
+++ netlify/functions/deploy-building.js
@@ -1,13 +1,6 @@
 const fetch = require('node-fetch');
 
 exports.handler = async (event, context) => {
-
-  console.log('starting the build')
-  try {
-    console.log('hitting the telegram API')
-    const resp = await fetch('https://v1.nocodeapi.com/kokaneka/telegram/OtoWBxzkiUHeAbge/sendText?text=building', { method: 'POST'});
-  } catch(e) {
-    console.log('error while hitting the telegram API', JSON.stringify(e));
-  }
+  const resp = await fetch('https://v1.nocodeapi.com/kokaneka/telegram/OtoWBxzkiUHeAbge/sendText?text=building', { method: 'POST'});
   console.log('hit the API!')
 }
