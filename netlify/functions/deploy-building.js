0288720465dea6c07c760077f81006fc007808c7
kokanek <kapeel.kokane@gmail.com>
2021-03-27 09:47:26 +0530

    changed the nocode endpoint

--- netlify/functions/deploy-building.js
+++ netlify/functions/deploy-building.js
@@ -1,6 +1,6 @@
 const fetch = require('node-fetch');
 
 exports.handler = async (event, context) => {
-  const resp = await fetch('https://v1.nocodeapi.com/kokaneka/telegram/OtoWBxzkiUHeAbge/sendText?text=building', { method: 'POST'});
+  const resp = await fetch('https://v1.nocodeapi.com/kokanek/telegram/enkhcbolRKGeTbEA/sendText?text=building', { method: 'POST'});
   console.log('hit the API!')
 }
