9b76814ae350a6a989a10398c9ea11c3273b1b18
kokanek <kapeel.kokane@gmail.com>
2021-03-27 10:05:07 +0530

    dummy commit

--- netlify/functions/deploy-building.js
+++ netlify/functions/deploy-building.js
@@ -2,5 +2,5 @@ const fetch = require('node-fetch');
 
 exports.handler = async (event, context) => {
   const resp = await fetch('https://v1.nocodeapi.com/kokanek/telegram/enkhcbolRKGeTbEA/sendText?text=building', { method: 'POST'});
-  console.log('hit the API!')
+  console.log('hit the build API!')
 }
