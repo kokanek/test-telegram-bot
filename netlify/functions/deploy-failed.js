a45471e8eefbd0b6df2cf09fc974a5279a792a9c
kokanek <kapeel.kokane@gmail.com>
2021-03-28 10:31:01 +0530

    removing the coded netlify hooks

--- netlify/functions/deploy-failed.js
+++
@@ -1,6 +0,0 @@
-const fetch = require('node-fetch');
-
-exports.handler = async (event, context) => {
-  const resp = await fetch('https://v1.nocodeapi.com/kokanek/telegram/enkhcbolRKGeTbEA/sendText?text=failed', { method: 'POST'});
-  console.log('hit the API!')
-}
